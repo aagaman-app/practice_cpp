30df544483947ffca151de47427fe8d011b6bdcd
Agam Arora <agam@v2g.io>
2020-05-27 17:43:44 +0530

    practice

--- test.cpp
+++ test.cpp
@@ -1,1 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int T;
+    cin >> T;
+    cout << setiosflags(ios::uppercase);
+    cout << setw(0xf) << internal;
+    while(T--)
+    {
+        double A;
+        cin >> A;
+        double B;
+        cin >> B;
+        double C;
+        cin >> C;
+
+
+        double y = (int)(B * 100.0 + 0.5);
+        double dy = (double)
+
+        int n = static_cast<int> (floor(A));
+        stringstream ss;
+        ss << hex << n;
+        string res = ss.str();
+
+        cout<<left<<"0x" + res<<endl;
+        char b;
+        B>=0? b = '+':'-';
+
+
+        cout<<setfill('_')<<setw(15)<<right<<b;
+        cout<<setprecision(3)<<B<<endl;
+
+
+
+    }
+    return 0;
+
+}
